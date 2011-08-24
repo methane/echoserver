d1ac4abfcb4fc244b4154505c505b993857728d1
INADA Naoki <songofacandy@gmail.com>
2011-08-24 19:25:46 +0900

    node.js 版で backlog の設定が出来ていなくて動かなかったのを修正.

--- echo_server_nodejs.js
+++ echo_server_nodejs.js
@@ -1,8 +1,4 @@
 var net = require('net');
 
-var server = net.createServer(function (socket) {
-  //socket.write("Echo server\r\n");
-  socket.pipe(socket);
-}, {'backlog': 1024})
-
+var server = net.createServer(function (socket) {socket.pipe(socket);});
 server.listen(5000, "0.0.0.0");
