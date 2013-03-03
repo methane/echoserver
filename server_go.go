d1c475c7091eca86c673388ea21f5d4e0d87753e
INADA Naoki <inada-n@klab.com>
2013-03-03 18:17:03 +0900

    go: go fmt

--- server_go.go
+++ server_go.go
@@ -1,27 +1,28 @@
 package main
+
 import (
-    "log";
-    "net";
-    "io";
+	"io"
+	"log"
+	"net"
 )
 
 func echo_handler(conn net.Conn) {
-    defer conn.Close();
-    io.Copy(conn, conn);
+	defer conn.Close()
+	io.Copy(conn, conn)
 }
 
 func main() {
-    psock, e := net.Listen("tcp", ":5000");
-    if e != nil {
-        log.Fatal(e);
-        return
-    }
-    for {
-        conn, e := psock.Accept();
-        if e != nil {
-            log.Fatal(e);
-            return
-        }
-        go echo_handler(conn);
-    }
+	psock, e := net.Listen("tcp", ":5000")
+	if e != nil {
+		log.Fatal(e)
+		return
+	}
+	for {
+		conn, e := psock.Accept()
+		if e != nil {
+			log.Fatal(e)
+			return
+		}
+		go echo_handler(conn)
+	}
 }
